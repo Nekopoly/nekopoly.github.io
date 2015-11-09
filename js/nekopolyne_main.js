4201fb816e2e96fe300b6b59a8df45d7b29bd017
Nekopoly <spinpo12@naver.com>
2015-11-09 17:40:30 +0900

    미디어쿼리 추가
    모바일페이지 리다이렉트안함.

--- js/nekopolyne_main.js
+++ js/nekopolyne_main.js
@@ -6,18 +6,6 @@ switch(Math.floor((Math.random() * 5) +1) ){
 	case 4 : $('#titletxt').text("네코폴리는 폴리폴리해."); break;
 	default : break;
 }
-
-
-/*switch(Math.floor((Math.random() * 5) +1) ){
-	case 1 : $('#text').css("font-size",30); $('#text').html("페이지가 이상하게<br/>보이는점을<br/>수정했어요."); break;
-	case 2 : $('#text').html("말풍선<br/>내용바뀌는거<br/>처음봤어요?"); break;
-	case 3 : $('#text').css("font-size",29); $('#text').html("더뜯어고치고 싶은데<br/>생각이 안나네요."); break;
-	case 4 : $('#text').css("font-size",40); $('#text').html("..."); break;
-	case 5 : $('#text').html("냐옹<br/>Meow"); break;
-	default : break;
-}*/
-
-
 	$('.polynyan').click(function(){
 		document.getElementById('nya').play()
 	});
