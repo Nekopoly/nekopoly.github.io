557bf82a70c378ae42e47855626f29e5cf08db9c
Nekopoly <spinpo12@naver.com>
2016-07-12 15:54:44 +0900

    auto complete link

--- js/index.js
+++ js/index.js
@@ -23,7 +23,8 @@ $(document).ready(function() {
   .done(function(data) {
     var posts = data;
     for (var i = 0; i < posts.length; i++) {
-      $('#append_t').append('<div class="cardview">'+posts[i].user.name+'<br>'+posts[i].text+'</div>');
+      //트위터 타임라인.
+      $('#append_t').append('<div class="cardview_content"><img src="'+posts[i].user.profile_image_url+'"/><span class="t_head tw_head_top">'+posts[i].user.name+'</span><hr><div class="tw_desc">'+posts[i].text+'</div></div>');
     }
   })
   .fail(function() {
@@ -31,7 +32,12 @@ $(document).ready(function() {
     console.log("error");
   })
   .always(function() {
-    $("#loading").remove();
+    $("#loading").fadeOut('slow', function() {
+          $("#loading").remove();
+    });
+         $('.cardview_content').autolink();
+         $('.cardview_content').mentionlink();
+         $('.cardview_content').hashtaglink();
     console.log("complete");
   });
 });
---
+++ js/jquery.autolink.js
@@ -0,0 +1,42 @@
+jQuery.fn.highlight = function (text, o) {
+	return this.each( function(){
+		var replace = o || '<span class="highlight">$1</span>';
+		$(this).html( $(this).html().replace( new RegExp('('+text+'(?![\\w\\s?&.\\/;#~%"=-]*>))', "ig"), replace) );
+			console.log('highlitght');
+	});
+}
+
+jQuery.fn.autolink = function (target) {
+    if (target == null) target = '_blank';
+	return this.each( function(){
+		var re = /((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g;
+		$(this).html( $(this).html().replace(re, '<a href="$1" target="'+ target +'">$1</a> ') );
+			console.log('AutoLink');
+	});
+}
+
+jQuery.fn.mailto = function () {
+	return this.each( function() {
+		var re = /(([a-z0-9*._+]){1,}\@(([a-z0-9]+[-]?){1,}[a-z0-9]+\.){1,}([a-z]{2,4}|museum)(?![\w\s?&.\/;#~%"=-]*>))/g;
+		$(this).html( $(this).html().replace( re, '<a href="mailto:$1">$1</a>' ) );
+			console.log('mailto');
+	});
+}
+
+jQuery.fn.hashtaglink = function (target) {
+    if (target == null) target = '_blank';
+	return this.each( function() {
+        var re = /#([^\s]+)/g;
+		$(this).html( $(this).html().replace( re, '<a href="https://twitter.com/search?q=%23$1" target="'+ target +'">#$1</a>' ) );
+			console.log('hashtaglink');
+	});
+}
+
+jQuery.fn.mentionlink = function (target) {
+    if (target == null) target = '_blank';
+	return this.each( function() {
+        var re = /@([^\s]+)/g;
+		$(this).html( $(this).html().replace( re, '<a href="https://twitter.com/$1" target="'+ target +'">@$1</a>' ) );
+		console.log('mentionlink');
+	});
+}
